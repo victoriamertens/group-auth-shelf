0364a24e5704bd5e8da2c8f71dab3f479e224efe
Dylan Roets <dylanroets@yahoo.com>
2022-12-15 10:46:31 -0600

    finished post

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const {rejectUnauthenticated} = require('../modules/authentication-middleware')
 
 // Get all shelved items
 router.get('/', (req, res) => {
@@ -12,11 +13,28 @@ router.get('/', (req, res) => {
 /**
  * Add an item for the logged in user to the shelf
  */
-router.post('/', (req, res) => {
+router.post('/', rejectUnauthenticated, (req, res) => {
   // receive object of {id: #, description: 'string', image_url: 'url', user_id: #}
+    //authoriztion : choose your own adventure for limits/divides
+    // if(req.user.access_level > 10)
+    console.log('/shelf POST route');
+    console.log(req.body);
+    console.log('is authenticated?', req.isAuthenticated());
+    console.log('user', req.user);
+    let queryText = `
+    INSERT INTO "item" ("description", "image_url", "user_id")
+    VALUES ($1, $2, $3);`
+    let queryValues = [req.body.description, req.body.image_url, req.user.id]
+    pool.query(queryText, queryValues)
+    .then(() => {
+    console.log('working item post')
+    res.sendStatus(200);
+    }).catch((err) => {
+        res.sendStatus(500)
+    })
+});
 
   // endpoint functionality
-});
 
 /**
  * Delete an item if it's something the logged in user added
