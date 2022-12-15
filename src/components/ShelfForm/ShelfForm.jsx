c4c982ebf2b759800c85e0c92dfaf99a9ce63d53
Victoria Mertens <victoria.mertens44@gmail.com>
2022-12-15 13:12:25 -0600

    shelfform done

--- src/components/ShelfForm/ShelfForm.jsx
+++ src/components/ShelfForm/ShelfForm.jsx
@@ -3,30 +3,36 @@ import { useDispatch } from 'react-redux';
 
 function ShelfForm() {
   const [description, setDescription] = useState('');
-  const [url, setUrl] = useState('');
+  const [image_url, setImage_Url] = useState('');
   const dispatch = useDispatch();
 
   function addItem() {
     event.preventDefault();
     console.log('in add item');
-    dispatch({ type: 'ADD_SHELF_ITEM', payload: { description, url } });
+    dispatch({ type: 'ADD_SHELF_ITEM', payload: { description, image_url } });
+    setDescription('');
+    setImage_Url('');
   }
   return (
     <form onSubmit={addItem}>
-      <input
-        onChange={(event) => {
-          setDescription(event.target.value);
-          console.log(description);
-        }}
-        type="text"
-        placeholder="Description"
-      ></input>
-      <input
-        onChange={(event) => setUrl(event.target.value)}
-        type="text"
-        placeholder="Image-URL"
-      ></input>
-      <button>Submit</button>
+      <div>
+        <input
+          onChange={(event) => {
+            setDescription(event.target.value);
+            console.log(description);
+          }}
+          type="text"
+          placeholder="Description"
+          value={description}
+        ></input>
+        <input
+          onChange={(event) => setImage_Url(event.target.value)}
+          type="text"
+          placeholder="Image-URL"
+          value={image_url}
+        ></input>
+        <button>Submit</button>
+      </div>
     </form>
   );
 }
